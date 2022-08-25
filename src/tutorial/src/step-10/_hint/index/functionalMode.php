3f687c48d3f8ec4b5006845c0e39a623b8907178
mychidarko <mickdd22@gmail.com>
2022-08-25 21:26:58 +0000

    feat: update leaf db example

--- src/tutorial/src/step-10/_hint/index/functionalMode.php
+++ src/tutorial/src/step-10/_hint/index/functionalMode.php
@@ -2,6 +2,13 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+db()->connect(
+  'eu-cdbr-west-03.cleardb.net',
+  'heroku_fb1311a639bb407',
+  'b9607a8a6d5ebb',
+  'cc589b17'
+);
+
 app()->get('/', function () {
   echo "hello world";
 });
