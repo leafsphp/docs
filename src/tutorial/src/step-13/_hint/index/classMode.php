89a65304064581c9dbdf432387744affbc3ecbce
mychidarko <mickdd22@gmail.com>
2022-10-29 23:08:08 +0000

    feat: added chapter 14

--- src/tutorial/src/step-13/_hint/index/classMode.php
+++ src/tutorial/src/step-13/_hint/index/classMode.php
@@ -4,12 +4,28 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = new Leaf\App();
 
-$app->get('/', function () {
-  echo "hello world";
-});
+$app->cors();
 
-$app->put('/custom', function () {
-  echo "custom route";
+$db = new Leaf\Db();
+$auth = new Leaf\Auth();
+
+$db->connect(
+  'eu-cdbr-west-03.cleardb.net',
+  'heroku_fb1311a639bb407',
+  'b9607a8a6d5ebb',
+  'cc589b17'
+);
+
+$app->post('/register', function () use ($app, $auth) {
+  $user = $auth->register(
+    $app->request()->get(['name', 'email', 'password'])
+  );
+
+  $app->response()->json([
+    'status' => 'success',
+    'message' => 'Registration successful',
+    'data' => $user
+  ]);
 });
 
 $app->run();
