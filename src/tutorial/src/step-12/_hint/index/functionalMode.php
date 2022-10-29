980239cb482a98e9aa6df2904472165aac11b34a
mychidarko <mickdd22@gmail.com>
2022-10-29 21:39:40 +0000

    feat: complete step 12

--- src/tutorial/src/step-12/_hint/index/functionalMode.php
+++ src/tutorial/src/step-12/_hint/index/functionalMode.php
@@ -2,12 +2,36 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-app()->get('/', function () {
-  echo "hello world";
-});
+db()->connect(
+  'eu-cdbr-west-03.cleardb.net',
+  'heroku_fb1311a639bb407',
+  'b9607a8a6d5ebb',
+  'cc589b17'
+);
 
-app()->put('/custom', function () {
-  echo "custom route";
+app()->get('/', function () {
+  db()
+    ->query('
+      DROP TABLE IF EXISTS users;
+      CREATE TABLE users (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        email VARCHAR(50),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      )
+    ')
+    ->execute();
+
+  db()
+    ->insert('users')
+    ->params(['name' => 'John Doe', 'email' => 'johndoe@test.com'])
+    ->execute();
+
+  $users = db()->select('users')->get();
+
+  response()->json([
+    'users' => $users,
+  ]);
 });
 
 app()->run();
