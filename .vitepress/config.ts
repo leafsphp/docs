4ff0582fab293dbdd6f91aaf90651a5794c02cfb
mychidarko <mickdd22@gmail.com>
2023-01-09 22:54:40 +0000

    fix: patch up component styles

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -637,6 +637,14 @@ export default defineConfigWithTheme<ThemeConfig>({
       }
     ],
     [
+      'style',
+      {},
+      fs.readFileSync(
+        path.resolve(__dirname, './inlined-scripts/global.css'),
+        'utf-8'
+      )
+    ],
+    [
       'link',
       {
         rel: 'preload',
@@ -713,14 +721,6 @@ export default defineConfigWithTheme<ThemeConfig>({
         href: 'https://fonts.googleapis.com/css?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500|DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700|Inter:300,400,500,600|Open+Sans:400,600;display=swap',
         rel: 'stylesheet'
       }
-    ],
-    [
-      'style',
-      {},
-      fs.readFileSync(
-        path.resolve(__dirname, './inlined-scripts/global.css'),
-        'utf-8'
-      )
     ],
     [
       'script',
