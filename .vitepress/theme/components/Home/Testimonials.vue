c4d8b0ad6711c2d61468a41e2509e9c83e2bc9b9
mychidarko <mickdd22@gmail.com>
2024-11-07 09:55:38 +0000

    feat: update testimonials

--- .vitepress/theme/components/Home/Testimonials.vue
+++ .vitepress/theme/components/Home/Testimonials.vue
@@ -7,7 +7,7 @@
 
       <div class="mx-auto mt-4 grid grid-cols-1 gap-4 text-sm sm:grid-cols-2 xl:grid-cols-4">
         <div v-for="(columnGroup, columnGroupIdx) in testimonials" :key="columnGroupIdx" class="grid gap-4"
-          :class="columnGroupIdx === 1 ? 'xl:col-span-2' : ''">
+          :class="columnGroupIdx === 1 ? 'hidden xl:grid xl:col-span-2' : ''">
           <div :class="columnGroupIdx === 1 ? 'xl:grid xl:grid-cols-2 gap-4' : ''">
             <Card v-for="testimonial in columnGroup" :key="testimonial.tagline"
               class="rounded-2xl p-6 shadow-lg ring-1 ring-gray-900/5 h-auto">
@@ -34,6 +34,16 @@ import Card from '../shared/Card.vue';
 const testimonials = [
   [
     {
+      body: `Today’s web development landscape demands tools that are fast, lightweight, and capable of growing with your projects. Many PHP frameworks try to do it all, making it challenging to find one that truly checks all these boxes—often adding unnecessary complexity to projects that need to stay nimble. That’s where Leaf shines. It gives the essentials to start building right away, with the flexibility to add exactly the features you need, precisely when you need them.`,
+      name: 'Andrew MacLean',
+      tagline: '@andrewdmaclean',
+      imageUrl:
+        'https://avatars.githubusercontent.com/u/26526271?v=4',
+      url: 'https://github.com/andrewdmaclean',
+    },
+  ],
+  [
+    {
       body: 'Thanks for making this! I have been looking for an alternative to Silex for a few years now. I have tried other frameworks like Lumen, Laravel & Slim, but they were always a pain to setup with my VMs, and usually forced you to adhere to a console generated bundle.',
       name: 'propeller-aaron',
       tagline: '@propeller-aaron',
@@ -41,15 +51,13 @@ const testimonials = [
         'https://avatars.githubusercontent.com/u/52088295?v=4',
       url: 'https://github.com/propeller-aaron',
     },
-  ],
-  [
     {
-      body: 'Awesome, I just found about it and already using it for a client\'s site.Clean, well organized and the MVC versión is just great!!!',
-      name: 'Carlos Cárdenas',
-      tagline: '@carlos_cardenas1',
-      url: 'https://www.producthunt.com/@carlos_cardenas1',
+      body: 'As a design/frontend-heavy agency, we needed a lightweight, but powerful framework that would just work on our servers without any hassle. Leaf has been a great choice for us as it allows us to build our APIs really fast, giving us time to focus on what we want: the design and experience of our projects.',
+      name: 'Rare Goods Only',
+      tagline: '@raregoodsonly',
+      url: 'https://raregoodsonly.webflow.io/',
       imageUrl:
-        'https://ph-avatars.imgix.net/2028422/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop&dpr=2',
+        'https://pbs.twimg.com/profile_images/1671450894448177160/4R8HRGR8_400x400.jpg',
     },
     {
       body: 'Awesome, I just found about it and already using it for a client\'s site.Clean, well organized and the MVC versión is just great!!!',
@@ -60,20 +68,12 @@ const testimonials = [
         'https://ph-avatars.imgix.net/2028422/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop&dpr=2',
     },
     {
-      body: 'Awesome, I just found about it and already using it for a client\'s site.Clean, well organized and the MVC versión is just great!!!',
-      name: 'Carlos Cárdenas',
-      tagline: '@carlos_cardenas1',
-      url: 'https://www.producthunt.com/@carlos_cardenas1',
+      body: 'Building with Leaf has been the most simple and straightforward approach to getting an app production ready',
+      name: 'Desmond Sofua',
+      tagline: '@cr34t1ve',
+      url: 'https://github.com/cr34t1ve',
       imageUrl:
-        'https://ph-avatars.imgix.net/2028422/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop&dpr=2',
-    },
-    {
-      body: 'Awesome, I just found about it and already using it for a client\'s site.Clean, well organized and the MVC versión is just great!!!',
-      name: 'Carlos Cárdenas',
-      tagline: '@carlos_cardenas1',
-      url: 'https://www.producthunt.com/@carlos_cardenas1',
-      imageUrl:
-        'https://ph-avatars.imgix.net/2028422/original?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=120&h=120&fit=crop&dpr=2',
+        'https://avatars.githubusercontent.com/u/37486330?v=4',
     },
   ],
   [
