e6721ad8bef07f6c85d337d6404763752aa01c8c
mychidarko <mickdd22@gmail.com>
2025-04-26 19:11:20 +0000

    feat: add selll.online ad

---
+++ .vitepress/theme/components/shared/Ad.vue
@@ -0,0 +1,13 @@
+<template>
+  <a href="https://selll.online" target="_blank" class="p-4 rounded-3xl bg-[var(--vp-c-bg-alt)] mt-5" id="widget">
+    <img src="https://selll.online/assets/img/screenshots/product.jpg" class="rounded-2xl" alt="Paperplane" width="457"
+      height="336">
+
+    <p class="mt-4 mb-2 text-xs">
+      <i>From the creators of Leaf PHP - </i>
+      Create your personalized storefront, add your products, share your link, and start sellling for free.
+    </p>
+
+    <small class="text-xs font-medium hover:underline">Learn more →</small>
+  </a>
+</template>
